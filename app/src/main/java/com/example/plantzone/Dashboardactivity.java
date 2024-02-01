6c194a0750f5018bb78f7c469f95542d36859584
RAJU-14 <pushparajraju2744@gmail.com>
2024-02-01 12:16:30 +0530

    dashboard pending

--- app/src/main/java/com/example/plantzone/Dashboardactivity.java
+++ app/src/main/java/com/example/plantzone/Dashboardactivity.java
@@ -1,14 +1,24 @@
 package com.example.plantzone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class Dashboardactivity extends AppCompatActivity  {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboardactivity);
+
+
+
     }
 }
--- app/src/main/java/com/example/plantzone/loginactivi.java
+++ app/src/main/java/com/example/plantzone/loginactivi.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+
+
 public class loginactivi extends AppCompatActivity implements View.OnClickListener {
     Button b3,b2;
     @Override
