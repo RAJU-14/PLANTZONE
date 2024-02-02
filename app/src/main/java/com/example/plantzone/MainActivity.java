dabf0579c1fa964efa8abf5ace99df371e410415
Roshinni <142069473+Roshinni@users.noreply.github.com>
2024-02-02 13:14:12 +0530

    progress

--- app/src/main/java/com/example/plantzone/MainActivity.java
+++ app/src/main/java/com/example/plantzone/MainActivity.java
@@ -6,24 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Handler handler;
     Button b1 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1 = (Button) findViewById(R.id.button);
-        b1.setOnClickListener(this);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent i = new Intent(MainActivity.this, secondactivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+
     }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.button) {
-
-
-                Intent i1 = new Intent(this, secondactivity.class);
-                startActivity(i1);
-            }
-    }
+        }
 }
