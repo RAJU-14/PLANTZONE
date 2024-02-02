e8ecf9e26ec0d65d6283a60190ed2c3f42b3c33e
RAJU-14 <pushparajraju2744@gmail.com>
2024-02-02 13:12:18 +0530

    feedbacl

--- app/src/main/java/com/example/plantzone/Dashboardactivity.java
+++ app/src/main/java/com/example/plantzone/Dashboardactivity.java
@@ -3,13 +3,17 @@ package com.example.plantzone;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class Dashboardactivity extends AppCompatActivity  {
+public class Dashboardactivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    ImageView b1, b2, b3, b4;
 
 
 
@@ -18,7 +22,24 @@ public class Dashboardactivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboardactivity);
 
+        b1 = (ImageView) findViewById(R.id.imgbtn1);
+        b2 = (ImageView) findViewById(R.id.imgbtn2);
+        b3 = (ImageView) findViewById(R.id.imgbtn3);
+        b4 = (ImageView) findViewById(R.id.imgbtn4);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
 
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.imgbtn4) {
+            Intent i = new Intent(this, FeedbackActivity.class);
+            startActivity(i);
+        }
 
     }
 }
